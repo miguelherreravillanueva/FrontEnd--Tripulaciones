9f75dfbfcb80d4f4bf0b10548f3b4fa47ccbc8ec
DanielMiguelez <danielmiguelez1993@gmail.com>
2023-01-18 15:55:24 +0100

    First commit

---
+++ src/features/auth/authService.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+
+const API_URL = "http://localhost:8000";
+
+const register = async (userData) => {
+  const res = await axios.post(API_URL + "/users/createUser", userData);
+  return res.data;
+};
+
+const authService = {
+  register,
+};
+
+export default authService;
